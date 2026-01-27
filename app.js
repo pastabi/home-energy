32fe4adcb41b3638086493af3bc6c217234fbfb1
Oleksii <gutsumori@gmail.com>
2026-01-28 00:43:29 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+async function checkEnergy() {
+  try {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 5000);
+    const responce = await fetch(process.env.HOME_URL, {
+      signal: controller.signal,
+    });
+
+    clearTimeout(timeoutId);
+
+    if (responce.status === 401) return true;
+    if (responce.status === 200) return true;
+    return false;
+  } catch (error) {
+    return false;
+  }
+}
+
+app.get("/", async (req, res) => {
+  const energyStatus = await checkEnergy();
+
+  const statusText = energyStatus ? "Світло є" : "Cвітла нема";
+  const statusStyle = energyStatus ? "color: green" : "color: red";
+
+  res.send(`
+    <h2 style="text-align: center; margin-top: 20px">Статус світла:</h2>
+    <h1 style="${statusStyle}; text-align: center">${statusText}</h1>`);
+});
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, console.log(`server is listening on port: ${port}`));
