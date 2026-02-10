5faf9824b7725e6be748807c27c11dafc12f04ed
Oleksii <gutsumori@gmail.com>
2026-02-10 02:31:39 +0200

    fix small time comparison edge case for old history entry

--- client/src/getData.ts
+++ client/src/getData.ts
@@ -69,13 +69,16 @@ function constructHistoryContentArray(): void {
 
   const coef = 4; // x minutes = 1px of height
   const maxHistoryReturnOldness = 1000 * 60 * 60 * 24 * 7; // 7 days
+  const theLastAcceptableHistoryEntryTimestamp: number = Date.now() - maxHistoryReturnOldness;
+  const startOfLastAcceptableDay: number = myStartOfDay(
+    new Date(theLastAcceptableHistoryEntryTimestamp),
+  ).getTime();
 
   let historyContent: HistoryEntryContent[] | OldHistoryEntryContent = [];
 
   if (
     currentStatus.history.length === 1 &&
-    new Date(currentStatus.history.at(0)!.dateOfChange).getTime() <
-      Date.now() - maxHistoryReturnOldness
+    new Date(currentStatus.history.at(0)!.dateOfChange).getTime() < startOfLastAcceptableDay
   ) {
     historyContent = {
       lastStatus: currentStatus.history.at(0)!.changedToStatus,
@@ -99,11 +102,6 @@ function constructHistoryContentArray(): void {
           thisStatus: { toStatus: changedToStatus, timeText: "", statusISOTime: "" },
           ifLastEntry: { lastEntryHeight: 0, lastEntryText: "" },
         };
-
-        const theLastAcceptableHistoryEntryTimestamp: number = Date.now() - maxHistoryReturnOldness;
-        const startOfLastAcceptableDay: number = myStartOfDay(
-          new Date(theLastAcceptableHistoryEntryTimestamp),
-        ).getTime();
 
         // filling dayChange
         for (let i = 0; i < daysStartTimestamps.length; i++) {
